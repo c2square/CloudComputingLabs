3a772027c9c540e837fb8e64ba0f85367f6dd849
csqure <544366139@qq.com>
2020-03-22 10:26:10 +0800

    线程部分：增加了线程的基本功能

--- Lab1/src/newSudoku/createThread.cpp
+++ Lab1/src/newSudoku/createThread.cpp
@@ -1,64 +1,45 @@
-#include<bits/stdc++.h>
-#include<windows.h>
-#include "sudoku.cpp"
-#include "SudokuSolve.cpp"
+#include <iostream>
+#include <string>
+#include "ThreadPool.h"
 using namespace std;
-static int  N =81;
-/**questionList问题列表
- * */
-queue<Sudoku> questionList;
-/**弹出第一个元素
- * 合成一个函数，使其原子化*/
-Sudoku fontAndPop(queue<Sudoku> &questionList){
-    Sudoku result=questionList.front();
-    questionList.pop();
-    return result;
-}
-/**创建随机数独
- * 样例，不用管
- * */
-int* createSudoku(){
-    static int result[81];
-    srand(time(0));
-    for (int i = 0; i < N; i++)
-    {
-        result[i]=rand()%10;
-    }
-    return result;
-}
-/**打印数独内容
- * 格式
- * {id,xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx}
- * */
-void printfSudoku(Sudoku result){
-    printf("{%s,",result.id.c_str());
-        for (int j = 0; j < N; j++)
-        {
-            // printf("%d",*(result.sudoku+j));
-        }
-        printf("}\n");
-}
-/**解数独函数
- * 随便写的，用于测试
- * */
 
-int main(){
-    /**Windows下shell编译运行c++文件的方法：
-     * 1. cd 到当前目录
-     * 2. 键入 g++ xxxx.cpp
-     * 3. 键入 ./a.exe
-     */
-    /** 生成问题列表
-     * */
-    for (int i = 0; i < 10; i++){
-        // Sudoku sudokuTmp={
-        //     to_string(i),
-        //     createSudoku()
-        // }; 
-        // questionList.push(sudokuTmp);
-        
+
+int main()
+{
+    std::mutex mtx;
+    try
+    {
+        std::ThreadPool tp;
+        std::vector<std::future<int>> v;
+        std::vector<std::future<void>> v1;
+        for (int i = 0; i <= 10; ++i)
+        {
+            auto ans = tp.add([](int answer) { return answer; }, i);
+            v.push_back(std::move(ans));
+        }
+        for (int i = 0; i <= 5; ++i)
+        {
+            auto ans = tp.add([&mtx](const std::string& str1, const std::string& str2)
+            {
+                std::lock_guard<std::mutex> lg(mtx);
+                std::cout << (str1 + str2) << std::endl;
+                return;
+            }, "hello ", "world");
+            v1.push_back(std::move(ans));
+        }
+        for (size_t i = 0; i < v.size(); ++i)
+        {
+            std::lock_guard<std::mutex> lg(mtx);
+            cout << v[i].get() << endl;
+        }
+        for (size_t i = 0; i < v1.size(); ++i)
+        {
+            v1[i].get();
+        }
     }
-    // sudokuSolve(questionList);
-    return 0;
-    
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
 }
